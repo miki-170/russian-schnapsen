a15074d740684be1b588b49b8d3aff8c6e678b51
miki-170 <lesiakmikolaj@gmail.com>
2026-07-22 00:13:27 +0200

    Added card images

--- schapsenGUI.py
+++ schapsenGUI.py
@@ -1,6 +1,7 @@
 import pygame
 from game_logic import Gamestate
 from cards import Suit, Rank
+from random import choice
 class GameGUI:
     
     def __init__(self):
@@ -12,14 +13,23 @@ class GameGUI:
         self.state = Gamestate()
         self.state.start_new_round()
         self._load_images()
-    
+        self.card_boxes = {}
+        
 
     # will fix it later
     def _load_images(self):
-        self.card_boxes = {}
+        names = { "9":"9","10":"10",'J':"jack",'Q':'queen','K':'king','A':'ace'}
+        self.card_images = {}
         for suit in Suit:
             for rank in Rank:
-                filename = f"cards/Playing Cards/SVG-cards-1.3/"
+                filename = f"cards/Playing Cards/PNG-cards-1.3/{names[rank.display]}_of_{str(suit.name).lower()}.png"
+                
+                try:
+                    image = pygame.image.load(filename)
+                    image = pygame.transform.scale(image,(70,100))
+                    self.card_images[(suit,rank)] = image
+                except:
+                    raise FileNotFoundError("File path doesn't exist")
 
     def run(self):
         running = True
@@ -31,7 +41,7 @@ class GameGUI:
                     running = False
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    self.state.start_new_round()
+                    self.handle_click(event.pos)
                 
             self.draw()
 
@@ -47,23 +57,42 @@ class GameGUI:
         # fills the background with poker green
         self.screen.fill((53, 101, 77))
         self.draw_hand()
-        self.draw_AI(0)
+        self.draw_ai(0)
+        self.draw_scores()
+
+    def handle_click(self, pos):
+        for card, rect in self.card_boxes.items():
+            if rect.collidepoint(pos):
+                legal = self.state.get_legal_moves(0)
+                print(card)
+                if card in legal:
+                    self.state.play_card(0,card)
+                    self.ai_move()
+                break
+    
+    def ai_move(self):
+        legal = self.state.get_legal_moves(1)
+        card = choice(legal)
+        self.state.play_card(1,card)
 
     def draw_hand(self):
-        self.card_boxes = {}
+        
         hand = self.state.hands[0]
 
         for i, card in enumerate(hand.cards):
             x = 65+ i * 90
             y = 550
+            
+            image = self.card_images[(card.suit, card.rank)]
+            self.screen.blit(image,(x,y))
+            
+            self.card_boxes[card] = pygame.Rect(x,y,70,100)
 
-            rect = pygame.Rect(x,y,70,100)
-            pygame.draw.rect(self.screen,(255,255,255),rect)
-            self.card_boxes[card] = rect
-
-    def draw_AI(self,show:bool):
-        self.card_boxes = {}
+    def draw_ai(self,show:bool):
+        
         hand = self.state.hands[1]
+
+        # also add an option to draw all the cards
         if show == False:
 
             image = pygame.image.load("cards/card_back.png")
@@ -71,10 +100,22 @@ class GameGUI:
             for i, card in enumerate(hand.cards):
                 x = 65+ i * 90
                 y = 50
-
-                rect = pygame.Rect(x,y,70,100)
                 self.screen.blit(image,(x,y))
-                self.card_boxes[card] = rect
+        else:
+            hand = self.state.hands[1]
+
+            for i, card in enumerate(hand.cards):
+                x = 65+ i * 90
+                y = 550
+                
+                image = self.card_images[(card.suit, card.rank)]
+                self.screen.blit(image,(x,y))
+                self.card_boxes[card] = pygame.Rect(x,y,70,100)
+                
+    def draw_scores(self):
+        font = pygame.font.SysFont(None, 30)
+        text = font.render(f"Turn: {self.state.current_player}; You: {self.state.scores[0]}  AI: {self.state.scores[1]}",True,(255,255,255))
+        self.screen.blit(text,(20,20))
 
 game = GameGUI()
 
